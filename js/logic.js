d9ded8ffba984a730a6caec01a8362438ccc7b87
laostirner <memaralicina@gmail.com>
2016-10-22 15:14:50 -0400

    Starting logic

---
+++ js/logic.js
@@ -0,0 +1,23 @@
+
+
+function executeMe()
+{
+    if (window.File && window.FileReader && window.FileList && window.Blob)
+    {
+        document.getElementById('image_input').onchange = function (e)
+        {
+         var fileRead = new FileReader();
+         var image_display = document.getElementById("image_display");
+         
+            
+        };
+        
+    }
+    
+    
+}
+
+
+
+
+executeMe();
